334d1e105f0b54f4aeb036712107b4be30e9524a
emonShimoul <emonshimoul@gmail.com>
2023-02-16 00:57:54 +0600

    findIndex

--- findIndex.js
+++ findIndex.js
@@ -1,3 +1,11 @@
 // array.prototype.findIndex()
 
-var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result = nums.findIndex((currentValue, index, arr) => {
+    // findIndex is exactly works as the find() method but instead of the first true element it will return its index. When the condition is not true for any of the element of the array it will return -1 
+    return currentValue >= 7;
+});
+
+console.log(result);
+

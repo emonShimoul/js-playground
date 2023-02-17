1145e76d9e6458981740da24badf5f8339be1936
emonShimoul <emonshimoul@gmail.com>
2023-02-18 00:05:16 +0600

    work with promise

---
+++ promise-2nd.js
@@ -0,0 +1,25 @@
+
+// const promise1 = Promise.resolve(`Promise 1 resolved`);
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(`Promise 1 resolved`);
+    }, 3000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(`Promise 2 resolved`);
+    }, 2000);
+});
+
+promise1.then((res) => console.log(res));
+promise2.then((res) => console.log(res));
+
+// Promise.all([promise1, promise2]).then((res) => {
+//     console.log(res);
+// });
+
+// Promise.race([promise1, promise2]).then((res) => {
+//     console.log(res);
+// });
---
+++ promise.js
@@ -0,0 +1,43 @@
+const hasMeeting = false;
+
+const meeting = new Promise((resolve, reject) => {
+    if (!hasMeeting) {
+        const meetingDetails = {
+            name: "Technical Meeting",
+            location: "Google Meet",
+            time: "10.00pm",
+        };
+        resolve(meetingDetails);
+    } else {
+        reject(new Error("Meeting already scheduled!!"));
+    }
+});
+
+const addToCalendar = (meetingDetails) => {
+    return new Promise((resolve, reject) => {
+        const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+        resolve(calendar);
+    });
+};
+
+// short form of using promise when there is no need of reject
+// const addToCalendar = (meetingDetails) => {
+//     const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+//     return Promise.resolve(calendar);
+// };
+
+meeting
+    .then(addToCalendar)
+    .then((res) => {
+        // resolved data
+        console.log(res);
+    })
+    .catch((err) => {
+        // rejected data
+        console.log(err.message);
+    })
+
+
+console.log("Hello...");
+console.log("Hello 1...");
+console.log("Hello 2...");

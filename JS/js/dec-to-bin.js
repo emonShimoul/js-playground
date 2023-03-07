0ffc3724f06e3ca29f0d54f40d2b5d8ba98afc84
emonShimoul <emonshimoul@gmail.com>
2023-03-08 03:22:25 +0600

    problem solving

---
+++ JS/js/dec-to-bin.js
@@ -0,0 +1,7 @@
+// decimal to binary
+console.log((48).toString(2));
+
+// binary to decimal
+// have to put 0b in front of binary number so that javascript can
+// understand that the given number is a binary number
+console.log((0b110000).toString(10));

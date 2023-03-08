27de2957cb05847e47e4a983c808b3ecc8f3910c
emonShimoul <emonshimoul@gmail.com>
2023-03-08 16:29:43 +0600

    problem solving

---
+++ problem-solving/count-vowels.js
@@ -0,0 +1,16 @@
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+function countVowels(sentence) {
+    let count = 0;
+    const letters = Array.from(sentence);
+
+    letters.forEach(function (value, index, array) {
+        if (vowels.includes(value)) {
+            count++;
+        }
+    });
+    return count;
+};
+
+console.log(countVowels("I love to code!!"));
+

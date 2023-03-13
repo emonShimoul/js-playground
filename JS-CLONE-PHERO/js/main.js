55d65d5d2ad81847041a4916a1f412064180c732
emonShimoul <emonshimoul@gmail.com>
2023-03-13 10:58:34 +0600

    phero template project

---
+++ JS-CLONE-PHERO/js/main.js
@@ -0,0 +1,30 @@
+const milestoneData = JSON.parse(data).data;
+// console.log(milestoneData);
+
+// load course milestone data
+function loadMilestones() {
+    const milestones = document.querySelector('.milestones');
+
+    milestones.innerHTML = `${milestoneData.map(function (milestone) {
+        return `<div class="milestone border-b">
+        <div class="flex">
+          <div class="checkbox"><input type="checkbox" /></div>
+          <div>
+            <p>
+              ${milestone.name}
+              <span><i class="fas fa-chevron-down"></i></span>
+            </p>
+          </div>
+        </div>
+        <div class="hidden_panel">
+          ${milestone.modules.map(function (module) {
+            return `<div class="module border-b">
+            <p>${module.name}</p>
+          </div>`;
+        }).join("")}
+        </div>
+      </div>`;
+    }).join("")}`;
+}
+
+loadMilestones();

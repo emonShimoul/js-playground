d88f1da770706b954d70a1a791b582f14ab318ae
emonShimoul <emonshimoul@gmail.com>
2023-03-24 03:41:27 +0600

    practice project in js

---
+++ PRACTICE-PROJECTS/geometry-genius/js/functions.js
@@ -0,0 +1,32 @@
+// get the value from input element
+function getInputValue(idName) {
+  return parseFloat(document.getElementById(idName).value);
+}
+
+function myFunction(name){
+  console.log(name);
+  console.log("clicked" + name);
+}
+
+// display the area calculations
+function displayAreaCalc(calculatedArea, name) {
+  const calculationBody = document.getElementById('calculation-body');
+  let counter = calculationBody.children.length;
+  const div = document.createElement('div');
+  div.setAttribute('id', 'myid');
+  const displayCalc = `<table style="width:100%">
+    <tr>
+      <td><small>${counter}.</small></td>
+      <td class="fs-6"><small>${name}</small></td>
+      <td><small>${calculatedArea.toFixed(2)}</small></td>
+      <td><a class="btn btn-primary btn-sm my-2 px-1" id="${counter}"><small>Convert to m<sup>2<sup></small></a></td>
+    </tr>
+  </table>`;
+  div.innerHTML = displayCalc;
+  calculationBody.appendChild(div);
+
+  // function convert();
+  document.getElementById(`${counter}`).addEventListener('click', function(){
+    console.log(`clicked ${name}`);
+  });
+}

cf5a256e325ec56d71af6cb5d87034a2d82e9d25
emonShimoul <emonshimoul@gmail.com>
2023-03-24 03:49:31 +0600

    calculation

--- PRACTICE-PROJECTS/geometry-genius/js/functions.js
+++ PRACTICE-PROJECTS/geometry-genius/js/functions.js
@@ -12,21 +12,21 @@ function myFunction(name){
 function displayAreaCalc(calculatedArea, name) {
   const calculationBody = document.getElementById('calculation-body');
   let counter = calculationBody.children.length;
+  let finalCalculatedArea = calculatedArea.toFixed(2);
   const div = document.createElement('div');
   div.setAttribute('id', 'myid');
   const displayCalc = `<table style="width:100%">
     <tr>
       <td><small>${counter}.</small></td>
       <td class="fs-6"><small>${name}</small></td>
-      <td><small>${calculatedArea.toFixed(2)}</small></td>
+      <td><small>${finalCalculatedArea}</small></td>
       <td><a class="btn btn-primary btn-sm my-2 px-1" id="${counter}"><small>Convert to m<sup>2<sup></small></a></td>
     </tr>
   </table>`;
   div.innerHTML = displayCalc;
   calculationBody.appendChild(div);
 
-  // function convert();
   document.getElementById(`${counter}`).addEventListener('click', function(){
-    console.log(`clicked ${name}`);
+    console.log(`result ${finalCalculatedArea * finalCalculatedArea}`);
   });
 }

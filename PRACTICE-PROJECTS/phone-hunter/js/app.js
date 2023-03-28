d42889c5af327d291c88a8f9296bcb4930c4f76e
emonShimoul <emonshimoul@gmail.com>
2023-03-28 21:08:33 +0600

    phone-hunter project practice

--- PRACTICE-PROJECTS/phone-hunter/js/app.js
+++ PRACTICE-PROJECTS/phone-hunter/js/app.js
@@ -1,15 +1,34 @@
-const loadPhones = async(searchText) => {
+const loadPhones = async(searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
-const displayPhones = phones => {
-    console.log(phones);
+const displayPhones = (phones, dataLimit) => {
+    // console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.textContent = "";
+
+    // display 10 phones only
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10){
+    phones = phones.slice(0, 10);
+    showAll.classList.remove('d-none');
+    } else{
+        showAll.classList.add('d-none');
+    }
+
+    // display no phone found
+    const noPhone = document.getElementById('no-phone-found');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    } else{
+        noPhone.classList.add('d-none');
+    }
+
+    // display all phone
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -19,18 +38,69 @@ const displayPhones = phones => {
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
             </div>
         </div>
         `;
 
         phonesContainer.appendChild(phoneDiv);
     });
+    // stop loader
+    toggleSpinner(false);
 }
 
-document.getElementById('btn-search').addEventListener('click', () => {
+const processSearch = (dataLimit) => {
+    toggleSpinner(true);
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
-    loadPhones(searchText);
+    loadPhones(searchText, dataLimit);
+}
+
+// handle button search click
+document.getElementById('btn-search').addEventListener('click', () => {
+    // start loader
+    processSearch(10);
 })
 
-loadPhones();
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function(e){
+    // console.log(e.key);
+    if(e.key === 'Enter'){
+        processSearch(10);
+    }
+})
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    } else{
+        loaderSection.classList.add('d-none');
+    }
+}
+
+// not the best way to load show all
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    processSearch();
+});
+
+const loadPhoneDetails = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone => {
+    console.log(phone);
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+        <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found!!'}</p>
+        <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Information Found!!'}</p>
+        <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth!!'}</p>
+    `;
+}
+
+loadPhones('apple');

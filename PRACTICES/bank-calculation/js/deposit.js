52f4ec6aacc1102e606fd942f1dc96bbca0b8078
emonShimoul <emonshimoul@gmail.com>
2023-11-19 23:47:17 +0600

    update deposit

--- PRACTICES/bank-calculation/js/deposit.js
+++ PRACTICES/bank-calculation/js/deposit.js
@@ -10,4 +10,10 @@ document.getElementById('btn-deposit').addEventListener('click', () => {
 
     const newDepositTotal = previousDepositTotal + newDepositAmount;
     depositTotalElement.innerText = newDepositTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
+
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
 })
---
+++ PRACTICES/bank-calculation/js/withdraw.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-withdraw').addEventListener('click', () => {
+    console.log('withdraw button clicked');
+})

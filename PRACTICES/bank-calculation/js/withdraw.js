de532402f0587375e9b0b923a1e1b23437851b4f
emonShimoul <emonshimoul@gmail.com>
2023-11-20 19:42:41 +0600

    calculate withdraw amount

--- PRACTICES/bank-calculation/js/withdraw.js
+++ PRACTICES/bank-calculation/js/withdraw.js
@@ -1,3 +1,18 @@
 document.getElementById('btn-withdraw').addEventListener('click', () => {
-    console.log('withdraw button clicked');
+
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmount = parseFloat(withdrawField.value);
+    withdrawField.value = "";
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotal = parseFloat(withdrawTotalElement.innerText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = newWithdrawTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
 })

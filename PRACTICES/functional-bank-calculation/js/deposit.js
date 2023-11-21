caf24795cf185092f750f88dd0ad6a048baec933
emonShimoul <emonshimoul@gmail.com>
2023-11-21 18:22:26 +0600

    get and set element value using a function

--- PRACTICES/functional-bank-calculation/js/deposit.js
+++ PRACTICES/functional-bank-calculation/js/deposit.js
@@ -1,18 +1,11 @@
-function getInputFieldValueById(inputFieldId){
-    const inputField = document.getElementById(inputFieldId);
-    const inputFieldValue = parseFloat(inputField.value);
-    inputField.value = '';
-
-    return inputFieldValue;
-}
-
-function getElementValueById(elementId){
-    const element = document.getElementById(elementId);
-    const elementValue = parseFloat(element.innerText);
-
-    return elementValue;
-}
 
 document.getElementById('btn-deposit').addEventListener('click', () => {
     const newDepositAmount = getInputFieldValueById('deposit-field');
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
 });

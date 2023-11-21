73cce6c3f3486ec0d3cc555ec0510f94ca8d2e2f
emonShimoul <emonshimoul@gmail.com>
2023-11-21 19:25:52 +0600

    handle decrease and update price

--- PRACTICES/shopping-cart/js/case.js
+++ PRACTICES/shopping-cart/js/case.js
@@ -1,8 +1,28 @@
-document.getElementById('btn-case-plus').addEventListener('click', () => {
+
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const previousCaseNumber = parseInt(caseNumberField.value);
-    const newCaseNumber = previousCaseNumber + 1;
+    
+    let newCaseNumber;
+    if(isIncrease){
+        newCaseNumber = previousCaseNumber + 1;
+    } else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
 
     caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', () => {
+    const newCaseNumber = updateCaseNumber(true);
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', () => {
+    updateCaseNumber(false);
 });
 

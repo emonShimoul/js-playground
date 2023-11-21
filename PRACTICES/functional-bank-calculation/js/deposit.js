8a45059d1d21a8ff41e9d22bc6eaaab5f9aed644
emonShimoul <emonshimoul@gmail.com>
2023-11-21 18:10:37 +0600

    use of function

--- PRACTICES/functional-bank-calculation/js/deposit.js
+++ PRACTICES/functional-bank-calculation/js/deposit.js
@@ -1,3 +1,18 @@
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValue = parseFloat(inputField.value);
+    inputField.value = '';
+
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValue = parseFloat(element.innerText);
+
+    return elementValue;
+}
+
 document.getElementById('btn-deposit').addEventListener('click', () => {
-    console.log('deposit button clicked!');
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 });

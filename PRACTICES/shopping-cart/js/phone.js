330deb919c5ea612184e36e37ed4da39d8a1b0ec
emonShimoul <emonshimoul@gmail.com>
2023-11-22 17:30:32 +0600

    set phone plus and minus button working

--- PRACTICES/shopping-cart/js/phone.js
+++ PRACTICES/shopping-cart/js/phone.js
@@ -1,7 +1,36 @@
-document.getElementById('btn-phone-plus').addEventListener('click', () => {
+function updatePhoneNumber(isIncrease){
     const phoneNumberField = document.getElementById('phone-number-field');
     const previousPhoneNumber = parseInt(phoneNumberField.value);
 
-    const newPhoneNumber = previousPhoneNumber + 1;
+    // const newPhoneNumber = previousPhoneNumber + 1;
+    
+
+    let newPhoneNumber;
+    if(isIncrease){
+        newPhoneNumber = previousPhoneNumber + 1;
+    } else{
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+
     phoneNumberField.value = newPhoneNumber;
+
+    return newPhoneNumber;
+}
+
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+document.getElementById('btn-phone-plus').addEventListener('click', () => {
+    const newPhoneNumber = updatePhoneNumber(true);
+
+    updatePhoneTotalPrice(newPhoneNumber);
+});
+
+document.getElementById('btn-phone-minus').addEventListener('click', () => {
+    const newPhoneNumber = updatePhoneNumber(false);
+
+    updatePhoneTotalPrice(newPhoneNumber);
 });

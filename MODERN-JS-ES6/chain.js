8cbb3c94fb7c030fb6cf819567d18a60f2b2466e
emonShimoul <emonshimoul@gmail.com>
2024-03-10 23:32:05 +0600

    optional chaining

---
+++ MODERN-JS-ES6/chain.js
@@ -0,0 +1,40 @@
+const data = [{id: 1, name: 'Emon', address: 'Khulna'}];
+
+// console.log(data[0].address);
+
+const products = {
+    count: 5000,
+    data: [
+        {id: 1, name: 'lenevo laptop', price: 30000},
+        {id: 2, name: 'macbook', price: 80000}
+    ]
+}
+
+// console.log(products.data[1].price);
+
+
+const user = {
+    id: 5004,
+    name: "Emon Shimoul",
+    address: {
+        street: {
+            first: "54/1 uttar side",
+            second: "KDA",
+            third: "Alor goli"
+        },
+        city: "Khulna"
+    }
+}
+
+const user2 = {
+    id: 5003,
+    name: "Fara Tuze",
+    address: {
+            first: "54/1 uttar side",
+            second: "KDA",
+            third: "Alor goli"
+    }
+}
+
+console.log(user.address.street?.second);
+console.log(user2.address.street?.second);

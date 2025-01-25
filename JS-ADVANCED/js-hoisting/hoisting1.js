75144a7135cc1fd40e106cc89e3fd2d4afb4130e
emonShimoul <emonshimoul@gmail.com>
2025-01-25 22:44:25 +0600

    another example of hoisting

--- JS-ADVANCED/js-hoisting/hoisting1.js
+++ JS-ADVANCED/js-hoisting/hoisting1.js
@@ -6,8 +6,15 @@ console.log(x);
 let x; // ReferenceError: Cannot access 'x' before initialization
 
 /**
+ *
  * let, const, var all are hoisted.
+ *
  * Variables defined with let and const are hoisted to the top of the block, but not initialized. Meaning: The block of code is aware of the variable, but it cannot be used until it has been declared. The variable is in a "temporal dead zone" from the start of the block until it is declared.
+ *
  * But var is initialized with 'undefined' by default.
+ *
+ * Using a let variable before it is declared will result in a ReferenceError.
+ *
+ * But using a const variable before it is declared, is a syntax error, so the code will simply not run. For const value of the variable must be assigned when it is declared otherwise it will throw a sytaxerror.
  *
  */
---
+++ JS-ADVANCED/js-hoisting/hoisting2.js
@@ -0,0 +1,13 @@
+/*
+function A() {
+  x = 5;
+  console.log(x);
+  let x;
+}
+
+A(); // output: Uncaught ReferenceError: Cannot access 'x' before initialization
+*/
+
+
+carName = "Volvo";
+const carName;   // Uncaught SyntaxError: Missing initializer in const declaration
